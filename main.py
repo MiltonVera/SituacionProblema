fb9a60fbde696ef228094713b88162ab58776470
MiltonVera <a00833471@itesm.mx>
2021-10-02 19:08:33 -0500

    milton

---
+++ main.py
@@ -0,0 +1,37 @@
+from os import system
+import time
+print("Bienvenido a tu mejor amigo para sacar la mejor calificacion de PISA")
+print()
+print()
+time.sleep(1)
+print("Tendras la oportunidad de aprender los temas que vienen en el examen de forma practica")
+time.sleep(2)
+print("Al final de cada tema tendras la oportunidad de practicarlo y ver si lo pudiste aprender correctamente")
+time.sleep(2)
+print("Cuando te sientas listo/a para realizar el examen PISA te realizaremos un examen final para que puedas comprobar que ya estas preparado para el examen")
+time.sleep(2)
+kk = input("Presiona enter para empezar")
+time.sleep(2)
+system("cls")
+while True:
+    print("Selecciona un opción para comenzar")
+    print("1.-Estudio general")
+    print("2.-Estudio especifico")
+    print("3.-Examen Final")
+    print("4.-Salir del programa")
+    opcion = input()
+    if(opcion == "1" or opcion == "Estudio general"):
+        pass
+    elif(opcion == "2" or opcion == "Estudio especifico"):
+        pass
+    elif(opcion == "3" or opcion == "Examen final"):
+        pass
+    elif(opcin == "4" or opcion == "Salir del programa"):
+        pass
+    else:
+        print("Hasta la proxima")
+        exit()
+
+
+
+
